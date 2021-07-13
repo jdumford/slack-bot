25364b29cde0cbfdc69a65c89817a3f33e799e60
Jacob Dumford <jacobdumford@jacobs-mbp-2.lan>
2021-07-13 16:11:33 -0400

    updated credentials module

---
+++ credentials.py
@@ -0,0 +1,7 @@
+SLACK_TOKEN = 'xoxb-XXXXXXXXXXXXX-XXXXXXXXXXXXX-XXXXXXXXXXXXXXXXXXXXXXXX'
+SIGNING_SECRET = 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx'
+USER_ID = 'UXXXXXXXXXX'
+
+JIRA_TOKEN = 'xxxxxxxxxxxxxxxxxxxxxxxx'
+JIRA_SERVER = 'https://SERVERNAME.atlassian.net/'
+JIRA_USER = 'smartdeveloper@gmail.com'
